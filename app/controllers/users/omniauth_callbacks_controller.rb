8da6d29d0b7bdba24cbbff064d4fc9959941a251
Norman Robert <normanrobertf@gmail.com>
2021-12-20 21:38:23 +0800

    added refresh logic for user

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -2,15 +2,22 @@
 
 class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
   def google_oauth2
-      # You need to implement the method below in your model (e.g. app/models/user.rb)
-      @user = User.from_omniauth(request.env['omniauth.auth'])
+    # You need to implement the method below in your model (e.g. app/models/user.rb)
+    @user = User.from_omniauth(request.env['omniauth.auth'])
 
-      if @user.save
-        flash[:notice] = I18n.t 'devise.omniauth_callbacks.success', kind: 'Google'
-        sign_in_and_redirect @user, event: :authentication
-      else
-        session['devise.google_data'] = request.env['omniauth.auth'].except('extra') # Removing extra as it can overflow some session stores
-        redirect_to new_user_registration_url, alert: @user.errors.full_messages.join("\n")
-      end
+    if @user.save
+      flash[:notice] = I18n.t 'devise.omniauth_callbacks.success', kind: 'Google'
+      credentials = request.env['omniauth.auth'].credentials
+      @user.update(
+        token: credentials['token'],
+        expires: credentials['expires'],
+        expires_at: credentials['expires_at'],
+        refresh_token: credentials['refresh_token']
+      )
+      sign_in_and_redirect @user, event: :authentication
+    else
+      session['devise.google_data'] = request.env['omniauth.auth'].except('extra') # Removing extra as it can overflow some session stores
+      redirect_to new_user_registration_url, alert: @user.errors.full_messages.join("\n")
+    end
   end
 end
