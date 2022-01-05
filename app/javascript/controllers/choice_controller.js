9e248532c0b50cc3493590a9c835da4639970a53
Norman Robert <normanrobertf@gmail.com>
2022-01-05 21:00:37 +0800

    added choice selection

---
+++ app/javascript/controllers/choice_controller.js
@@ -0,0 +1,25 @@
+// Visit The Stimulus Handbook for more details
+// https://stimulusjs.org/handbook/introduction
+//
+// This example controller works with specially annotated HTML like:
+//
+// <div data-controller="hello">
+//   <h1 data-target="hello.output"></h1>
+// </div>
+
+import { Controller } from "stimulus"
+
+export default class extends Controller {
+  static targets = [ "startDate", "endDate", "location" ]
+
+  // No trigger event needed as we initialize a tooltip using tippy.js where trigger event is included
+
+  selection() {
+    $(".choice").removeClass("clicked")
+    event.currentTarget.classList.add("clicked")
+    this.startDateTarget.value = event.params["start"]
+    this.endDateTarget.value = event.params["end"]
+    this.locationTarget.value = event.params["location"]
+  }
+
+}
