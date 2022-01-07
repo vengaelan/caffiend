a846ad5c55aaaad72a42553c761542ae0cc47711
Venga Elan <vengaelan@gmail.com>
2022-01-07 15:11:13 +0800

    new meeting page only shows choices

--- app/models/meeting.rb
+++ app/models/meeting.rb
@@ -5,7 +5,7 @@ class Meeting < ApplicationRecord
   accepts_nested_attributes_for :choices, reject_if: :all_blank, allow_destroy: true
 
   # Validations
-  validates :start_datetime, :end_datetime, presence: true
+  validates :start_datetime, :end_datetime, presence: true, :if => :invitee_email?
   validates_format_of :invitee_email, :with => /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i, :allow_blank => true
   attribute :status, :string, default: 'PENDING'
   validates :status, inclusion: { in: ["PENDING", "ACCEPTED", "CANCELLED", "COMPLETED"] }
