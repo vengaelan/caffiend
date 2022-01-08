d9b87da88229b687c5763d960cea70ab164b2f9f
Huixin Tan <huixintan91@gmail.com>
2022-01-08 11:30:08 +0800

    changed navbar and footer color, made landing page responsive and animated content

--- app/javascript/components/navbar.js
+++ app/javascript/components/navbar.js
@@ -2,7 +2,7 @@ const initUpdateNavbarOnScroll = () => {
   const navbar = document.querySelector('.navbar-lewagon');
   if (navbar) {
     window.addEventListener('scroll', () => {
-      if (window.scrollY >= window.innerHeight) {
+      if (window.scrollY >= 50) {
         navbar.classList.add('navbar-lewagon-color');
       } else {
         navbar.classList.remove('navbar-lewagon-color');
