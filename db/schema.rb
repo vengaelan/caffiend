8414435e44781f682c8c9b09c3006c2c9a723010
Huixin Tan <huixintan91@gmail.com>
2022-01-08 14:18:43 +0800

    add about and description to Users

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_12_29_111655) do
+ActiveRecord::Schema.define(version: 2022_01_08_061402) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -55,6 +55,8 @@ ActiveRecord::Schema.define(version: 2021_12_29_111655) do
     t.boolean "expires"
     t.string "refresh_token"
     t.string "image"
+    t.string "about"
+    t.text "description"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
