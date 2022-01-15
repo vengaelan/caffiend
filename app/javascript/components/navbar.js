6cc93aa75d6dacb7b665491795b79494581153d6
Huixin Tan <huixintan91@gmail.com>
2022-01-15 15:27:14 +0800

    navbar change animation

--- app/javascript/components/navbar.js
+++ app/javascript/components/navbar.js
@@ -2,7 +2,7 @@ const initUpdateNavbarOnScroll = () => {
   const navbar = document.querySelector('.navbar-lewagon');
   if (navbar) {
     window.addEventListener('scroll', () => {
-      if (window.scrollY >= 50) {
+      if (window.scrollY >= window.innerHeight) {
         navbar.classList.add('navbar-lewagon-color');
       } else {
         navbar.classList.remove('navbar-lewagon-color');
