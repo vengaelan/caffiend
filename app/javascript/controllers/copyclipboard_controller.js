afae0ebb8ba78daf0e15c7dc1bd1edb251e4555a
Venga Elan <vengaelan@gmail.com>
2022-01-16 14:13:09 +0800

    cleaning up code

--- app/javascript/controllers/copyclipboard_controller.js
+++ app/javascript/controllers/copyclipboard_controller.js
@@ -3,8 +3,6 @@ import { Controller } from "stimulus"
 export default class extends Controller {
   static targets = ["url"]
 
-  // No trigger event needed as we initialize a tooltip using tippy.js where trigger event is included
-
   copy() {
     this.urlTarget.select();
     document.execCommand('copy');
